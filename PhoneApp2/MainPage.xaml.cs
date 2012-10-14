e5d6b8c842af2702f187c6b50ad04f108d3c81d0
aywengo <aywengo@gmail.com>
2012-10-14 19:23:23 +0300

    Init commit

---
+++ PhoneApp2/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Text;
+
+namespace PhoneApp2
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCalculate(object sender, RoutedEventArgs e)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            IPAddress ipAdr;
+            int maskLenght;
+            if (IPAddress.TryParse(IP.Text, out ipAdr) && int.TryParse(MaskLength.Text, out maskLenght) && maskLenght < 31)
+            {
+                var snm = SubnetMask.CreateByNetBitLength(maskLenght);
+                int maxHostAmount = (2 << (31 - maskLenght)) - 2;
+
+                builder.Append(string.Format("Your IP: {0} \n", IP.Text));
+                builder.Append(string.Format("Subnet mask: {0} \n", snm));
+                builder.Append(string.Format("Network IP: {0} \n", ipAdr.GetNetworkAddress(snm)));
+                builder.Append(string.Format("Network broadcast IP: {0} \n",  ipAdr.GetBroadcastAddress(snm)));
+                builder.Append(string.Format("Max hosts amount: {0} \n", maxHostAmount));
+            }
+            else
+            {
+                builder.Append("Ups, some unknown error has been occured :)");                
+            }
+
+            Result.Text = builder.ToString();
+        }
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            IP.Text = string.Empty;
+            MaskLength.Text = string.Empty;
+            Result.Text = string.Empty;
+        }
+    }
+}
---
+++ PhoneApp2/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LAN IP Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LAN IP Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0892dd2b-0811-4feb-b18c-349f995903d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
