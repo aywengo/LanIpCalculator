53c1a5376f462c695a776b81971d853af2278b8a
aywengo <aywengo@gmail.com>
2012-10-15 20:18:08 +0300

    Added calculate menuitem

--- LanIpCalculator/MainPage.xaml.cs
+++ LanIpCalculator/MainPage.xaml.cs
@@ -16,6 +16,18 @@ namespace LanIpCalculator
 
         private void OnCalculate(object sender, RoutedEventArgs e)
         {
+            this.Calculate();
+        }
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            IP.Text = string.Empty;
+            MaskLength.Text = string.Empty;
+            Result.Text = string.Empty;
+        }
+
+        private void Calculate()
+        {
             StringBuilder builder = new StringBuilder();
 
             IPAddress ipAdr;
@@ -28,23 +40,21 @@ namespace LanIpCalculator
                 builder.Append(string.Format("Host IP: {0} \n", IP.Text));
                 builder.Append(string.Format("Subnet mask: {0} \n", snm));
                 builder.Append(string.Format("Network IP: {0} \n", ipAdr.GetNetworkAddress(snm)));
-                builder.Append(string.Format("Network broadcast IP: {0} \n",  ipAdr.GetBroadcastAddress(snm)));
+                builder.Append(string.Format("Network broadcast IP: {0} \n", ipAdr.GetBroadcastAddress(snm)));
                 builder.Append(string.Format("Max hosts amount: {0} \n", maxHostAmount));
-                builder.Append(string.Format("IP range: {0} - {1} \n", ipAdr.GetFirstSubnetAddress(snm) , ipAdr.GetLastSubnetAddress(snm)));
+                builder.Append(string.Format("IP range: {0} - {1} \n", ipAdr.GetFirstSubnetAddress(snm), ipAdr.GetLastSubnetAddress(snm)));
             }
             else
             {
-                builder.Append("Ups, some unknown error has been occured :)");                
+                builder.Append("Ups, some unknown error has been occured :)");
             }
 
             Result.Text = builder.ToString();
         }
 
-        private void OnClear(object sender, EventArgs e)
+        private void ApplicationBarMenuItem_Click(object sender, EventArgs e)
         {
-            IP.Text = string.Empty;
-            MaskLength.Text = string.Empty;
-            Result.Text = string.Empty;
+            this.Calculate();
         }
     }
 }
